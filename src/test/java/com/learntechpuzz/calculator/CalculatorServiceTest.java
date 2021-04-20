3474bdf5bc0d2b935a037165177b723f62299529
anbukarthick92 <82492262+anbukarthick92@users.noreply.github.com>
2021-04-20 17:26:05 +0530

    corrected changes 3

--- src/test/java/com/learntechpuzz/calculator/CalculatorServiceTest.java
+++ src/test/java/com/learntechpuzz/calculator/CalculatorServiceTest.java
@@ -15,7 +15,7 @@ class CalculatorServiceTest {
 
 	@Test
 	public void testAdd() {
-		assertThat(calculatorService.add(1, 2)).isEqualTo(5);
+		assertThat(calculatorService.add(1, 2)).isEqualTo(3);
 	}
 
 }
